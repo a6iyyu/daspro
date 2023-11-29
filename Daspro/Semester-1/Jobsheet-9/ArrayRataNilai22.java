b03a96060e5e4a70949a09a81e07cb51a768eb80
a6iyyu <ffst4irs@gmail.com>
2023-11-29 13:46:43 +0700

    Y

---
+++ Daspro/Semester-1/Jobsheet-9/ArrayRataNilai22.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class ArrayRataNilai22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilai_mahasiswa;
+        int lulus = 0, tidak_lulus = 0, jumlah_nilai_lulus = 0;
+        double total = 0, rata_nilai_lulus = 0, rata_nilai_tidak_lulus = 0;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int n = sc.nextInt();
+        nilai_mahasiswa = new int[n];
+        for (int i = 0; i < nilai_mahasiswa.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilai_mahasiswa[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilai_mahasiswa.length; i++) {
+            total += nilai_mahasiswa[i];
+            if (nilai_mahasiswa[i] > 70) {
+                jumlah_nilai_lulus += nilai_mahasiswa[i];
+                lulus++;
+            } else
+                tidak_lulus++;
+        }
+        rata_nilai_lulus = jumlah_nilai_lulus / lulus;
+        rata_nilai_tidak_lulus = (total - jumlah_nilai_lulus) / tidak_lulus;
+        System.out.println("Rata-rata nilai mahasiswa yang lulus = " + rata_nilai_lulus);
+        System.out.println("Rata-rata nilai mahasiswa yang tidak lulus = " + rata_nilai_tidak_lulus);
+        sc.close();
+    }
+}
