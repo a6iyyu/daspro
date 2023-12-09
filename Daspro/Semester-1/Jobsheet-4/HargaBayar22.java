0cfb14c546ab2df410206e59d62d0b58607b7d23
a6iyyu <ffst4irs@gmail.com>
2023-12-09 17:13:38 +0700

    Ok

--- Daspro/Semester-1/Jobsheet-4/HargaBayar22.java
+++ Daspro/Semester-1/Jobsheet-4/HargaBayar22.java
@@ -3,9 +3,9 @@ public class HargaBayar22 {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             double diskon = 0.1, total, bayar, jumlah_diskon;
-            System.out.println("Masukkan harga barang yang dibeli ");
+            System.out.print("Masukkan harga barang yang dibeli: ");
             int harga = input.nextInt();
-            System.out.println("Masukkan jumlah barang yang dibeli: ");
+            System.out.print("Masukkan jumlah barang yang dibeli: ");
             int jumlah = input.nextInt();
             total = harga * jumlah;
             jumlah_diskon = total * diskon;
