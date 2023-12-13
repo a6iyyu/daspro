7d3e5151f985d20aac87e7d6a93cb18bb7858c5b
a6iyyu <ffst4irs@gmail.com>
2023-12-13 08:26:17 +0700

    Tugas kuliah

--- Daspro/Semester-1/Jobsheet-10/Bioskop22.java
+++ Daspro/Semester-1/Jobsheet-10/Bioskop22.java
@@ -13,5 +13,8 @@ public class Bioskop22 {
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i + 1 + ": " + String.join(", ", penonton[i])));
+        }
     }
 }
--- Daspro/Semester-1/Jobsheet-10/BioskopWithScanner22.java
+++ Daspro/Semester-1/Jobsheet-10/BioskopWithScanner22.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class BioskopWithScanner22 {
     public static void main(String[] args) {
-        String nama, next;
-        int baris, kolom;
         Scanner sc = new Scanner(System.in);
+        int baris, kolom;
+        String nama, next;
         String[][] penonton = new String[4][2];
         while (true) {
             System.out.println("Menu:");
-            System.out.println("1. Input data penonton");
+            System.out.println("1. Masukkan data penonton");
             System.out.println("2. Tampilkan daftar penonton");
             System.out.println("3. Exit");
             System.out.print("Pilih menu (1/2/3): ");
-            int choice = sc.nextInt();
+            int menu = sc.nextInt();
             sc.nextLine();
-            switch (choice) {
+            switch (menu) {
                 case 1:
                     System.out.print("Masukkan nama: ");
                     nama = sc.nextLine();
@@ -21,38 +21,55 @@ public class BioskopWithScanner22 {
                     baris = sc.nextInt();
                     System.out.print("Masukkan kolom: ");
                     kolom = sc.nextInt();
-                    if (penonton[baris - 1][kolom - 1] == null) {
-                        penonton[baris - 1][kolom - 1] = nama;
-                        System.out.println("Data berhasil disimpan");
-
+                    sc.nextLine();
+                    if (baris >= 1 && baris <= penonton.length && kolom >= 1 && kolom <= penonton[0].length) {
+                        if (penonton[baris - 1][kolom - 1] == null) {
+                            penonton[baris - 1][kolom - 1] = nama;
+                            System.out.println("Data penonton berhasil dimasukkan.");
+                        } else {
+                            System.out.println("Kursi sudah terisi, pilih kuris lain!");
+                            System.out.print("Masukkan baris: ");
+                            baris = sc.nextInt();
+                            System.out.print("Masukkan kolom: ");
+                            kolom = sc.nextInt();
+                            sc.nextLine();
+                            if (penonton[baris - 1][kolom - 1] == null) {
+                                penonton[baris - 1][kolom - 1] = nama;
+                                System.out.println("Data penonton berhasil dimasukkan.");
+                            } else {
+                                System.out.println("Maaf, kursi sudah terisi. Pembatalan input.");
+                            }
+                        }
                     } else {
-                        System.out.println("Kursi sudah diisi oleh " + nama + " Silahkan mengisi kursi lain");
+                        System.out.println("Nomor baris atau kolom tidak valid, silakan coba lagi!");
                     }
                     break;
                 case 2:
+                    System.out.println("Daftar penonton: ");
                     for (int i = 0; i < 4; i++) {
                         for (int j = 0; j < 2; j++) {
-                            if (penonton[i][j] != null) {
-                                System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": " + penonton[i][j]);
+                            String nilai = penonton[i][j];
+                            if (nilai == null) {
+                                System.out.println("Baris ke-" + (i + 1) + ", kolom ke-" + (j + 1) + ": ***");
                             } else {
-                                System.out.println("Baris " + (i + 1) + ", Kolom " + (j + 1) + ": ***");
+                                System.out.println("Baris ke-" + (i + 1) + ", kolom ke-" + (j + 1) + ": " + penonton[i][j]);
                             }
                         }
                     }
                     break;
                 case 3:
+                    System.out.println("Terima kasih sudah menggunakan layanan kami!\nKeluar dari program.");
                     sc.close();
                     System.exit(0);
-                    break;
                 default:
                     System.out.println("Pilihan tidak valid. Silakan pilih 1, 2, atau 3.");
+                    break;
             }
             System.out.print("Kembali ke menu? (y/n): ");
-            next = sc.next();
+            next = sc.nextLine();
             if (next.equalsIgnoreCase("n")) {
                 break;
             }
-            sc.nextLine();
         }
     }
 }
