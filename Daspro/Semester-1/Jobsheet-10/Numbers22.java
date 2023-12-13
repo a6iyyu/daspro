ba392df783c70c7570268ce312b2a4d90e59e925
a6iyyu <ffst4irs@gmail.com>
2023-12-13 08:49:58 +0700

    Tugas kuliah

--- Daspro/Semester-1/Jobsheet-10/Numbers22.java
+++ Daspro/Semester-1/Jobsheet-10/Numbers22.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
 public class Numbers22 {
     public static void main(String[] args) {
-        int[][] my_numbers = new int[3][];
-        my_numbers[0] = new int[5];
-        my_numbers[1] = new int[3];
-        my_numbers[2] = new int[1];
+        int max_length = 9;
+        int[][] my_numbers = new int[6][max_length];
         for (int i = 0; i < my_numbers.length; i++) {
             System.out.println(Arrays.toString(my_numbers[i]));
-        } for (int i = 0; i < my_numbers.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + my_numbers[i].length);
         }
-}
+        for (int i = 0; i < my_numbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i + 1) + ": " + my_numbers[i].length);
+        }
+    }
 }
