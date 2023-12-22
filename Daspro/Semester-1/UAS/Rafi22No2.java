4d03b00a226fe999013c587be23542777a866bc4
a6iyyu <ffst4irs@gmail.com>
2023-12-22 09:48:54 +0700

    Kuis 2 Prakdaspro

---
+++ Daspro/Semester-1/UAS/Rafi22No2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Rafi22No2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan kalimat: ");
+        String kalimat = sc.nextLine();
+        if (palindrome(kalimat)) {
+            System.out.println("Kalimat tersebut termasuk palindrome.");
+        } else {
+            System.out.println("Kalimat tersebut bukanlah palindrome.");
+        } sc.close();
+    }
+    private static boolean palindrome(String validasi) {
+        validasi = validasi.replaceAll(" ", "").toLowerCase();
+        return validasi.equals(new StringBuilder(validasi).reverse().toString());
+    }
+}
